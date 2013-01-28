d64d1dacd1745d03137399c49dc4bc539ec59063
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-01-28 14:49:22 +0200

    key bindings: Mod+F12 locks screen
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- rc.lua
+++ rc.lua
@@ -275,7 +275,9 @@ globalkeys = awful.util.table.join(
                   awful.util.getdir("cache") .. "/history_eval")
               end),
     -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end)
+    awful.key({ modkey }, "p", function() menubar.show() end),
+    -- Marquiz's additions
+    awful.key({ modkey,           }, "F12",     function () awful.util.spawn("xscreensaver-command --lock") end)
 )
 
 clientkeys = awful.util.table.join(
