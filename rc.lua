c90b5fc0f32ed8a28a9f0bae543c1e943393089d
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-11-21 16:29:29 +0200

    Set number of master windows to 2
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- rc.lua
+++ rc.lua
@@ -171,6 +171,11 @@ awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
+    -- Set number of master windows for all tags
+    for _, t in pairs(s.tags) do
+        awful.tag.setnmaster(2, t)
+    end
+
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
