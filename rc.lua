07d8675aae732be2748faadb927088f05f4cf6b7
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2015-07-07 16:59:54 +0300

    Make Meld a floating window
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- rc.lua
+++ rc.lua
@@ -654,6 +654,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Gitk" },
       properties = { floating = true } },
+    { rule = { class = "Meld" },
+      properties = { floating = true } },
 }
 -- }}}
 
