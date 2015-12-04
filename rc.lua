3c2682c7929e0cf43dd3884dbb6a814349a2dac1
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2015-12-04 14:23:30 +0200

    Make LibreOffice windows floating
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- rc.lua
+++ rc.lua
@@ -518,6 +518,7 @@ awful.rules.rules = {
           "Gitk",
           "Gpick",
           "Kruler",
+          "libreoffice",
           "Meld",
           "MessageWin",  -- kalarm.
           "Sxiv",
